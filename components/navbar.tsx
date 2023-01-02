08a38bdcd5eebba63015d475787040a4007f5d2a
lior-gishry <lior.gishri@workiz.com>
2023-01-02 11:42:51 +0200

    menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ import { useRouter } from 'next/router'
 
 const Navbar: FC<NavBarProps> = (props) => {
   const router = useRouter()
-const { pathname, asPath, query } = router
+  const { pathname, asPath, query } = router
   const items = props.menuItems.items;
   const listItems = items.map((item) =>
     <li key={item.key}>
@@ -23,11 +23,11 @@ const { pathname, asPath, query } = router
   );
   listItems.push(
     <Link href="javascript:void(0)"
-    onClick={()=>router.push({ pathname, query }, asPath, { locale: 'he' })
-  }
-  >
-    'עב'
-  </Link>
+      onClick={() => router.push({ pathname, query }, asPath, { locale: props.locale == 'he' ? 'en' : 'he' })
+      }
+    >
+      {props.locale == 'he' ? 'en' : 'עב'}
+    </Link>
   )
   return (
     <div className={styles.wrapper}> <ul className={styles.horizontalMenu}>{listItems}</ul></div>
