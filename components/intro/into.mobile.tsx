41722b6575230bd3f57901b1f354ff9ef4307a7a
lior-gishry <lior.gishri@workiz.com>
2023-02-03 22:42:02 +0200

    intro

--- components/intro/into.mobile.tsx
+++ components/intro/into.mobile.tsx
@@ -3,19 +3,8 @@ import Image from 'next/image'
 import styles from 'components/home/home.mobile.module.css'
 const IntroMobile: FC<any> = ({  }) => {
   return (
-    <div className={styles.main}>
-         <div className={styles['image-wrap']}>
-      <Image
-        alt="Mountains"
-        src="https://res.cloudinary.com/lior/image/upload/v1675196681/noya2022/assets/6ddb5b_1d6982e2b09e497895d9cd759833133c_mv2.jpg"
-        fill
-        sizes="100vw"
-        style={{
-          objectFit: 'cover',
-        }}
-      />
-    </div>
-
+    <div className={styles['intro-wrap']}>
+        
     </div>
   )
 };
