44332a89b36a98cf4c0b72e74d3e78892bc65c6b
lior-gishry <lior.gishri@workiz.com>
2023-03-30 19:49:29 +0300

    book page

--- pages/book.tsx
+++ pages/book.tsx
@@ -6,6 +6,9 @@ import Navbar from '../components/navbar'
 import { GeneralSiteProps } from '../const'
 import Concerts from './concerts';
 import { useRouter } from 'next/router'
+import HomeCardMobile from 'components/home_card/home.card'
+import book from 'public/book.jpg'
+import wall from 'public/wall.jpg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,8 @@ const Book: FC<GeneralSiteProps> = (props) => {
   
   return (
     <>
-      <h1>Book page</h1>
+           <HomeCardMobile description='drumshouse_desc' img={book} img2={wall} generalSiteProps={props} title='drumshouse_title'></HomeCardMobile>
+
     </>
   )
 }
